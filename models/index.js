da0e27c8be32dad5ea8bee6628e818a08b05e4a6
Vontemilly <dmbootcampucf@gmail.com>
2024-02-06 21:59:56 -0500

    Created js for handlebars.

--- models/index.js
+++ models/index.js
@@ -21,28 +21,6 @@ Diet.belongsTo(User, {
     foreignKey: 'user_id'
 });
 
-User.hasMany(WarmUp, {
-  foreignKey: 'user_id'
-});
 
-WarmUp.belongsTo(Workout, {
-  foreignKey: 'user_id'
-});
-
-WarmUp.hasMany(Workout, {
-  foreignKey: 'user_id'
-});
-
-User.hasMany(CoolDown, {
-  foreignKey: 'user_id'
-});
-
-CoolDown.belongsTo(Workout, {
-  foreignKey: 'user_id'
-});
-
-CoolDown.hasMany(Workout, {
-  foreignKey: 'user_id'
-});
 
 module.exports = { User, Workout, Diet, WarmUp, CoolDown };
