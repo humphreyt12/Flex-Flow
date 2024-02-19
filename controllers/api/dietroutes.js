5a14660040d39fbad7c72a82ed8660f64a41ba32
Vontemilly <dmbootcampucf@gmail.com>
2024-02-18 21:52:45 -0500

    diet routes

--- controllers/api/dietroutes.js
+++ controllers/api/dietroutes.js
@@ -2,6 +2,29 @@ const router = require('express').Router();
 const { Diet } = require('../../models');
 const withAuth = require('../../utils/auth');
 
+//GET request for all diets
+router.get('/', (req,res) => {
+    Diet.findAll({})
+    .then(dietData => res.json(dietData))
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err)
+    });
+});
+// GET request by id
+router.get('/:id', (req, res) => {
+    Diet.findAll({
+            where: {
+                id: req.params.id
+            }
+        })
+        .then(DietData => res.json(DietData))
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        })
+});
+
 //POST request for new diet
 router.post('/', async (req, res) => {
   try {
@@ -9,9 +32,9 @@ router.post('/', async (req, res) => {
       ...req.body,
       user_id: req.session.user_id,
     });
-    res.status(200).json(newDiet);
+    res.json(newDiet);
   } catch (err) {
-    res.status(400).json(err);
+    res.status(500).json(err);
   }
 });
 
@@ -34,6 +57,4 @@ router.delete('/:id', withAuth, async (req, res) => {
     res.status(500).json(err);
   }
 });
-
-
 module.exports = router;
